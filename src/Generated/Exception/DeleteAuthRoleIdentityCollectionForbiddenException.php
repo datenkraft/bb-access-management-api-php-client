6f1c3930c3591328342d25d4cf1ae130dd2d8bc9
Andreas <andreas.uretschnig@datenkraft.com>
2021-09-26 16:43:56 +0200

    BB-1323 - regenerated client + composer update

--- src/Generated/Exception/DeleteAuthRoleIdentityCollectionForbiddenException.php
+++ src/Generated/Exception/DeleteAuthRoleIdentityCollectionForbiddenException.php
@@ -2,12 +2,12 @@
 
 namespace Datenkraft\Backbone\Client\AccessManagementApi\Generated\Exception;
 
-class GetAuthRoleIdentityCollectionBadRequestException extends BadRequestException
+class DeleteAuthRoleIdentityCollectionForbiddenException extends ForbiddenException
 {
     private $errorResponse;
     public function __construct(\Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse $errorResponse)
     {
-        parent::__construct('Bad Request', 400);
+        parent::__construct('Forbidden', 403);
         $this->errorResponse = $errorResponse;
     }
     public function getErrorResponse()
