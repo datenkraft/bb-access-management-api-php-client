2b8b8d0824c567327c7a6f052ecd209cb68c8dcb
CircleCI <office@datenkraft.com>
2023-04-27 14:12:34 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/GetAuditLogCollectionUnauthorizedException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\AccessManagementApi\Generated\Exception;
+
+class GetAuditLogCollectionUnauthorizedException extends UnauthorizedException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Unauthorized');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
