7e71587b984223522cb2d403f6320f88fb641f5c
CircleCI <office@datenkraft.com>
2023-05-04 15:23:48 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/PostAuthPermissionRoleCollectionConflictException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\AccessManagementApi\Generated\Exception;
+
+class PostAuthPermissionRoleCollectionConflictException extends ConflictException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Conflict');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
