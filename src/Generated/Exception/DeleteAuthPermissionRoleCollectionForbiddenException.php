6762a2ab91f64a31602195425553a10793b6a0b1
CircleCI <office@datenkraft.com>
2023-05-15 14:42:00 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/DeleteAuthPermissionRoleCollectionForbiddenException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\AccessManagementApi\Generated\Exception;
+
+class DeleteAuthPermissionRoleCollectionForbiddenException extends ForbiddenException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Forbidden');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\AccessManagementApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
